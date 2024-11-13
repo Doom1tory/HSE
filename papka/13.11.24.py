9c5d205370f81b491c5d2c01cedc2d6aeae5044a
Doom1tory <dmitrymarshalko@mail.ru>
2024-11-13 22:56:55 +0300

    done two types of exercises

--- papka/13.11.24.py
+++ papka/13.11.24.py
@@ -1,29 +1,71 @@
-#Задачи для решения от иишки
-##Задача 1: Работа со строками
-#Создайте строку, которая содержит ваше имя и фамилию, а затем выведите её в верхнем регистре.
-#Решение:
-#name = "Иванов Иван" первая
-#print(name.upper())
+# Задачи для решения от иишки
+# Задача 1: Работа со строками
+# Создайте строку, которая содержит ваше имя и фамилию, а затем выведите её в верхнем регистре.
+# Решение:
+# name = "Иванов Иван" первая
+# print(name.upper())
 
-##Создайте строку с названием вашего любимого фильма и выведите его в нижнем регистре. 
-#Решение
-#film = "Platoon"
-#print(film.lower())
+# Создайте строку с названием вашего любимого фильма и выведите его в нижнем регистре.
+# Решение
+# film = "Platoon"
+# print(film.lower())
 
-#city = "Moscow"
-#lenght = len(city)
-#print(lenght)
+# city = "Moscow"
+# lenght = len(city)
+# print(lenght)
 
-##Создайте строку с цитатой и замените одно слово на другое.
-#quote = "To be or not to be?"
-#modified_quote = quote.replace( "be", "and")
-#print(modified_quote)
+# Создайте строку с цитатой и замените одно слово на другое.
+# quote = "To be or not to be?"
+# modified_quote = quote.replace( "be", "and")
+# print(modified_quote)
 
-##Создайте новую строку с другой цитатой и замените в ней слово на синоним.
-##Попробуйте заменить несколько слов в одной строке, используя метод .replace() несколько раз.
-#quote1 = "If you see me, repeat"
-#modifiedQuote1 = quote1.replace("me", "us")
-#modifiedQuote2 = modifiedQuote1.replace("If", "When")
-#print(modifiedQuote2) 
+# Создайте новую строку с другой цитатой и замените в ней слово на синоним.
+# Попробуйте заменить несколько слов в одной строке, используя метод .replace() несколько раз.
+# quote1 = "If you see me, repeat"
+# modifiedQuote1 = quote1.replace("me", "us")
+# modifiedQuote2 = modifiedQuote1.replace("If", "When")
+# print(modifiedQuote2)
+
+# Задача 2: Сложение чисел
+# Задача: Напишите программу, которая складывает два числа и выводит результат.
+# Решение:
+# python
+# a = 5
+# b = 10
+# result = a + b
+# print(result)
+
+# Напишите программу, которая вычитает одно число из другого.
+# Напишите программу, которая умножает два числа.
+# Напишите программу, которая делит одно число на другое и выводит результат.
+
+# apples = 5
+# bananas = 10
+# fruits = apples + bananas
+# print(fruits)
+
+# apples = 5
+# bananas = 10
+# fruits = apples * bananas
+# print(fruits)
+
+# apples = 5
+# bananas = 10
+# fruits = apples / bananas
+# print(fruits)
+
+# Задача 3: Списки
+# Задача: Создайте список из пяти чисел и выведите третий элемент списка.
+# Решение:
+# python
+numbers = [1, 2, 3, 4, 5]
+print(numbers[2])
 
 
+fruits = [bananas, apples, hurma, tangerines, peach, watermelon]
+longfruits = len(fruits)
+print(fruits[5])
+# Дополнительные задачи:
+# Создайте список из ваших любимых фруктов и выведите последний элемент.
+# Измените второй элемент списка на новое значение.
+# Добавьте еще одно число в конец списка и выведите весь список.
