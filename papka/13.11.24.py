4d33ff5b4e8ff7815d3954334b8b8415219dc6f6
Doom1tory <dmitrymarshalko@mail.ru>
2024-11-13 17:55:08 +0300

    задачи, решенные днем 13.11.24

--- papka/13.11.24.py
+++ papka/13.11.24.py
@@ -1,2 +1,29 @@
+#Задачи для решения от иишки
+##Задача 1: Работа со строками
+#Создайте строку, которая содержит ваше имя и фамилию, а затем выведите её в верхнем регистре.
+#Решение:
 #name = "Иванов Иван" первая
 #print(name.upper())
+
+##Создайте строку с названием вашего любимого фильма и выведите его в нижнем регистре. 
+#Решение
+#film = "Platoon"
+#print(film.lower())
+
+#city = "Moscow"
+#lenght = len(city)
+#print(lenght)
+
+##Создайте строку с цитатой и замените одно слово на другое.
+#quote = "To be or not to be?"
+#modified_quote = quote.replace( "be", "and")
+#print(modified_quote)
+
+##Создайте новую строку с другой цитатой и замените в ней слово на синоним.
+##Попробуйте заменить несколько слов в одной строке, используя метод .replace() несколько раз.
+#quote1 = "If you see me, repeat"
+#modifiedQuote1 = quote1.replace("me", "us")
+#modifiedQuote2 = modifiedQuote1.replace("If", "When")
+#print(modifiedQuote2) 
+
+
